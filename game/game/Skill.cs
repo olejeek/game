19809317422f5f35e277d1d8dd5787bfa19c3bfe
Клезович Олег <allejeek@gmail.com>
2016-10-19 16:07:09 +0300

    Change the structure of the work skills. Now all actions is a skill, and executed in location class.

--- game/game/Skill.cs
+++ game/game/Skill.cs
@@ -38,20 +38,70 @@ namespace game
         }
     }
 
+    class Move:PersonalSkill
+    {
+        internal Move(Person who):base(who, who)
+        {
+            damage = 0;
+            enabled = true;
+        }
+        internal override void SkillEffect()
+        {
+            switch (targetCast.Direction)
+            {
+                case Person.WorldSide.N: targetCast.pos.newCoord(0, 1); break;
+                case Person.WorldSide.NE: targetCast.pos.newCoord(1, 1); break;
+                case Person.WorldSide.E: targetCast.pos.newCoord(1, 0); break;
+                case Person.WorldSide.SE: targetCast.pos.newCoord(1, -1); break;
+                case Person.WorldSide.S: targetCast.pos.newCoord(0, -1); break;
+                case Person.WorldSide.SW: targetCast.pos.newCoord(-1, -1); break;
+                case Person.WorldSide.W: targetCast.pos.newCoord(-1, 0); break;
+                case Person.WorldSide.NW: targetCast.pos.newCoord(-1, 1); break;
+            }
+            Console.WriteLine("Mob #{0} go to coord:{1}", targetCast.locId, targetCast.pos);
+            enabled = false;
+        }
+    }
     class PhisAtack:PersonalSkill
     {
-        internal int hit;
         internal bool critical;
         internal PhisAtack(Person whoCast, Person targetCast):base(whoCast, targetCast)
         {
             enabled = true;
-            timeDelay = 0;
-            critical = whoCast.r.Next(100) >= (100 - whoCast.crit);
             damage = whoCast.atack;
-            hit = whoCast.hit;
+            critical = whoCast.r.Next(100) >= (100 - whoCast.crit);
+            timeDelay = 0;
         }
         internal override void SkillEffect()
         {
+            if (whoCast.r.Next(100) > (100 - targetCast.pdodge))
+                Console.WriteLine("Mob #{0} perfect dodged the atack mob #{1}",
+                    targetCast.locId, whoCast.locId);
+            else if (critical)
+            {
+                targetCast.hp -= damage;
+                Console.WriteLine("Mob #{0} inflicted critical damage on mob #{1} to {2} hp",
+                    whoCast.locId, targetCast.locId, damage);
+            }
+            else
+            {
+                int atackHit = whoCast.hit;
+
+                if (targetCast.r.Next(atackHit) > atackHit - targetCast.flee)
+                {
+                    int d = damage - targetCast.def;
+                    //d = d > 0 ? d : 1;
+                    //hp -= d;
+                    targetCast.hp -= (d = d > 0 ? d : 1);
+                    Console.WriteLine("Mob #{0} damaged the mob #{1} to {2} hp",
+                        whoCast.locId, targetCast.locId, d);
+                }
+                else
+                {
+                    Console.WriteLine("Mob #{0} missed the mob #{1}",
+                        whoCast.locId, targetCast.locId);
+                }
+            }
             enabled = false;
         }
     }
